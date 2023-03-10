10fe32824ed22dbb61d22247b91bfab5613ec6ce
hristo-darlyanov <memeda963@gmail.com>
2023-03-10 13:49:55 +0200

    Added VotingClassifier

--- mlAlgorithms.py
+++ mlAlgorithms.py
@@ -1,6 +1,6 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn import metrics
@@ -37,8 +37,10 @@ def trained_LogisticRegression(X_train, X_test, Y_train, Y_test):
     return gs_best
 
 def trained_RandomForestClassifier(X_train, X_test, Y_train, Y_test):
-    rfc = RandomForestClassifier()
-    rfc_params = {'n_estimators': np.arange(100, 200, 10), 'max_depth': np.arange(1,20)}
+    #best max_depth determined to be 1
+    #will not be adding it to the GridSearchCV because its taking too long to compute
+    rfc = RandomForestClassifier(max_depth=1)
+    rfc_params = {'n_estimators': np.arange(100, 200, 10)}
     gs = GridSearchCV(rfc, rfc_params, cv=5)
     gs.fit(X_train, Y_train)
     gs_best = gs.best_estimator_
@@ -50,3 +52,17 @@ def trained_RandomForestClassifier(X_train, X_test, Y_train, Y_test):
     metrics.ConfusionMatrixDisplay.from_estimator(gs_best, X_test, Y_test)
 
     return gs_best
+
+def trained_VotingClassifier(lr_model, knn_model, rfc_model, X_train, X_test, Y_train, Y_test):
+    estimators = [('lr', lr_model), ('knn', knn_model), ('rfc', rfc_model)]
+    
+    vc = VotingClassifier(estimators=estimators, voting='hard')
+    vc.fit(X_train, Y_train)
+
+    yhat = vc.predict(X_test)
+
+    print(classification_report(Y_test, yhat))
+    metrics.ConfusionMatrixDisplay.from_estimator(vc, X_test, Y_test)
+
+    return vc
+    
